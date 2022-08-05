9fe6183c6cbbaa38f0563b352ce8b8b4b89363a2
BlueAngler <dannyo1028@gmail.com>
2022-08-04 22:28:58 -0500

    added 'if' statements to user prompts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var lowercasechoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var uppercasechoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberchoices = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialcharacterschoices = ["%", "#", "@","^", "&", "*", "(", ")", "!"];
-
+var choices = []
 
 
 // Assignment Code
@@ -27,12 +27,21 @@ generateBtn.addEventListener("click", writePassword);
 //}
 
 //function UserEntry(){
-var charLenght = prompt("Choose a password lenght greater than 8 and less than 128 characteres"); 
+ 
 
 
+//prompts user will go through when generating password
+function userchoices(){
+  var Lenghtpassword = prompt("Choose a password lenght greater than 8 and less than 128 characteres");
+  var lowercase=confirm("include lowercases in password?")
+    if(lowercase) {choices=choices.concat(lowercasechoices)}
+  var uppercase=confirm("include uppercases in password?")
+    if(uppercase) {choices=choices.concat(uppercasechoices)}
+  var numbers=confrim("include numbers in password?")
+  if(numbers){choices=choices.concat(numberchoices)}
+  var specialscharacters=confirm("include special characters in password? ")
+  if(specialcharacters) {choices=choices.concat(specialcharacterschoices)}
+}
+//called function to see if prompts work
+userchoices()
 
-
-var lowercase=confirm("include lowercases in password?")
-var uppercase=confirm("include uppercases in password?")
-var numbers=confrim("include numbers in password?")
-var specialscharacters=confirm("include special characters in password? ")
