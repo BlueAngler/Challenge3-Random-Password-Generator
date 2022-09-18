c8dac6f7bc9a79e0186473c37bc893ace534ab0c
BlueAngler <dannyo1028@gmail.com>
2022-09-17 20:30:29 -0500

    added prompts and updated array

--- script.js
+++ script.js
@@ -1,34 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var choices = ["A", "B", "C"]
+var lowerChoices = ["a", "b", "c"]
+var upperChoices = ["A", "B", "C"]
+var specialChoices = ["%", "$", "!"]
+var numericChoices = ["1", "2", "3"]
 var question1 = "password?"
 var question2= "lenght between 8 and 128 characters"
 var question3= "lower case?"
 var question4 = "upper case?"
 var question5= "numeric?"
 var question6="special characters?"
+var userChoice= []; 
 
 // Write password to the #password input
 function writePassword() {
-  // console.log("hi");
-  // function quest1() {
-  //   window.prompt(question1);
-  //   if(question1){
-  //   // console.log("good answer to quetsoins1");
-  //   }
-  // }
-  // quest1();
+  // 
+    var question1Response=window.confirm(question1);
+    if(!question1Response){
+      return;
+    }
+    
+  //  
 
   // function quest2() {
-    window.prompt(question2);
-    if (question2 < 8) {
+    var question2Response=window.prompt(question2);
+    if (question2Response < 8 || question2Response > 128) {
       
-    window.alert("needs longer");
+    window.alert("passwords needs to be longer than 8 characters or shorter than 128");
+    return;
+    }else {
+      userChoice.push(question2Response); 
+      console.log(userChoice); 
     }
 
 
-  // }
-  // quest2();
+    var question3Response=window.confirm(question3);
+    if(question3Response){
+      
+      userChoice.push(lowerChoices);
+      console.log("thanks for comfirming lower case");
+      console.log(userChoice);
+      
+    }
+
+
+    var question4Response=window.confirm(question4);
+    if(question4Response){
+      console.log("thanks for comfirming upper case")
+      userChoice.push(upperChoices);
+    }
+
+    
+    var question5Response=window.confirm(question5);
+    if(question5Response){
+      console.log("thanks for choosing numeric")
+      userChoice.push(numericChoices);
+    }
+
+
+
+    
+    var question5Response=window.confirm(question5);
+    if(question5Response){
+      console.log("thanks for comfirming upper case")
+      userChoice.push(specialChoices);
+    }
+    
+
+
+    //I need to generate a random password lenght that takes in the value of question1response as it's lenght, and randomly selectes characters based of of the updated userChoice array.
+
+   
 
   
 
