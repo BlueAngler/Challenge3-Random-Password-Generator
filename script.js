8d91e3b851d1377a9a08635a21588c68eafca9bb
BlueAngler <dannyo1028@gmail.com>
2022-09-17 19:06:46 -0500

    changed jscript prompts syntax

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-var lowercasechoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercasechoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberchoices = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialcharacterschoices = ["%", "#", "@","^", "&", "*", "(", ")", "!"];
-var choices = []
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var choices = ["A", "B", "C"]
+var question1 = "password?"
+var question2= "lenght between 8 and 128 characters"
+var question3= "lower case?"
+var question4 = "upper case?"
+var question5= "numeric?"
+var question6="special characters?"
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // console.log("hi");
+  // function quest1() {
+  //   window.prompt(question1);
+  //   if(question1){
+  //   // console.log("good answer to quetsoins1");
+  //   }
+  // }
+  // quest1();
 
-  passwordText.value = password;
+  // function quest2() {
+    window.prompt(question2);
+    if (question2 < 8) {
+      
+    window.alert("needs longer");
+    }
+
+
+  // }
+  // quest2();
+
+  
+
+  
+ 
+  
+ 
+  
+  // var password = generatePassword() 
+    
+    
+  
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
- 
-
-
-//prompts user will go through when generating password
-function userchoices(){
-  var Lenghtpassword = prompt("Choose a password lenght greater than 8 and less than 128 characteres");
-  var lowercase=confirm("include lowercases in password?")
-    if(lowercase) {choices=choices.concat(lowercasechoices)}
-  var uppercase=confirm("include uppercases in password?")
-    if(uppercase) {choices=choices.concat(uppercasechoices)}
-  var numbers=confrim("include numbers in password?")
-  if(numbers){choices=choices.concat(numberchoices)}
-  var specialscharacters=confirm("include special characters in password? ")
-  if(specialcharacters) {choices=choices.concat(specialcharacterschoices)}
-}
-//called function to see if prompts work
-userchoices()
-
